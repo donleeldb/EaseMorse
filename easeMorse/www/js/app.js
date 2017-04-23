922885d64682a9346743f6c91dea694e93133d0b
Siping Li <chinatoplee@gmail.com>
2017-04-23 00:34:19 -0500

    Update app.js
    create welcome state and set it as the default state

--- easeMorse/www/js/app.js
+++ easeMorse/www/js/app.js
@@ -30,7 +30,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-
+  // once opened, display a welcome page first
+    .state('welcome', {
+      url: "/welcome",
+      templateUrl: "templates/welcome.html",
+      controller: 'WelcomeCtrl'
+    })
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
@@ -83,6 +88,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/encode');
-
+  $urlRouterProvider.otherwise('/welcome');
 });
